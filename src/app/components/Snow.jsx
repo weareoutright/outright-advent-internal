1707b04581bb0b5e0811d0aa90aa5cc100f3bf7e
Dakota Fabro <dakota@weareoutright.com>
2023-11-29 18:37:35 -0800

    Lay foundation for snow increase

--- src/app/components/Snow.jsx
+++ src/app/components/Snow.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+'use client'
+
+import React, {useEffect, useState} from 'react'
 import Image from "next/image";
 import SNOW from "../images/snow/snow.svg"
 
 const Snow = () => {
+
+const [snowHeight, setSnowHeight] = useState(5)
+
+// useEffect(() => {
+//   const interval = setInterval(() => {
+//     setSnowHeight(snowHeight + 1);
+//   }, 86400000);
+
+//   return () => clearInterval(interval);
+// });
+
   return (
     <div className="Snow">
         <Image src={SNOW} alt=""></Image>
-        <div className="extra-snow"><span></span></div>
+        <div className="extra-snow" style={{height:`${snowHeight}em`}}><span></span></div>
     </div>
   )
 }
