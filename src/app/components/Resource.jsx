99c696b153dbe4d188713279bb77e0f23caeea00
Dakota Fabro <dakota@weareoutright.com>
2023-11-29 18:29:54 -0800

    Add href links for each resource

--- src/app/components/Resource.jsx
+++ src/app/components/Resource.jsx
@@ -5,9 +5,8 @@ import React from 'react'
 const Resource = ({id, title, href, resourceImg, orientation, releaseDate}) => {
     const today = new Date(Date.now()).toLocaleString('en-us', {timeZone: 'America/New_York'})
 
-    // add logic for covered resource vs uncovered - overlay?
   return (
-        <Link className={`div${id} box ${orientation} ${today >= releaseDate ? "" : "covered"}`} href={href} target='_blank' rel='noreferrer' alt={`link to ${title}`}>
+        <Link className={`div${id} box ${orientation} ${today >= releaseDate ? "" : "covered"}`} href={href} target='_blank' rel='noreferrer' alt={`link to ${title}`}style={today >= releaseDate ? {} : {"pointer-events": "none"}}>
         </Link>
   )
 }
