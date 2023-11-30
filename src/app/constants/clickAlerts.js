eb92d04dc16a898c20e9f1340967cd5a0df31615
Dakota Fabro <dakota@weareoutright.com>
2023-11-30 15:12:49 -0800

    Update click alerts - minor typos

--- src/app/constants/clickAlerts.js
+++ src/app/constants/clickAlerts.js
@@ -3,7 +3,7 @@ export const CLICK_ALERTS = [
   "Whoa getting a little ahead of yourself here, friend. Timer 👆",
   "If you're not Sarah, you may go. But if you ARE Sarah... Sarah! Please! I AM AT THE MERCY OF THE TIMER 👆",
   "patience [ pey-shuhns ] (noun): the quality of being patient, as the bearing of provocation, annoyance, misfortune, or pain, without complaint, loss of temper, irritation, or the like",
-  "patience [ pey-shuhns ] (noun): an ability or willingness to suppress restlessness or annoyance when confronted with delay:",
+  "patience [ pey-shuhns ] (noun): an ability or willingness to suppress restlessness or annoyance when confronted with delay",
   "Now now, friend. You must wait just like the rest.",
   "If you send Grace three compliments, I'll unlock this for you.",
   "If you send Dakota three compliments, I'll unlock this for you.",
