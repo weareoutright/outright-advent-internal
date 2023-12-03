2aafe7445d9cd84fabba24fb86fb8f8e1293fb3e
Dakota Fabro <dakotafabrodev@gmail.com>
2023-12-02 19:01:44 -0800

    Update resourceInfo.js

--- src/app/constants/resourceInfo.js
+++ src/app/constants/resourceInfo.js
@@ -1,4 +1,6 @@
+
 import day1 from "../images/resource-thumbnails/day1.svg";
+/*
 import day2 from "../images/resource-thumbnails/day2.svg";
 import day3 from "../images/resource-thumbnails/day3.svg";
 import day4 from "../images/resource-thumbnails/day4.svg";
@@ -14,7 +16,7 @@ import day13 from "../images/resource-thumbnails/day13.svg";
 import day14 from "../images/resource-thumbnails/day14.svg";
 import day15 from "../images/resource-thumbnails/day15.svg";
 import day16 from "../images/resource-thumbnails/day16.svg";
-
+*/
 import singleGridCover from "../images/resource-covers/single-grid-resource-cover.svg";
 import horizontalGridCover from "../images/resource-covers/horizontal-resource-cover.svg";
 import verticalGridCover from "../images/resource-covers/vertical-resource-cover.svg";
@@ -33,7 +35,7 @@ export const RESOURCES = [
     id: 3,
     title: "httpster",
     href: "https://httpster.net/",
-    resourceImg: "",
+    resourceImg: singleGridCover,
     coverImg: singleGridCover,
     orientation: "single",
     releaseDate: "12/4/2023, 9:00:00 AM",
@@ -42,7 +44,7 @@ export const RESOURCES = [
     id: 4,
     title: "shape-factory",
     href: "https://shapefactory.co/",
-    resourceImg: day3,
+    resourceImg: verticalGridCover,
     coverImg: verticalGridCover,
     orientation: "vertical",
     releaseDate: "12/5/2023, 9:00:00 AM",
@@ -51,7 +53,7 @@ export const RESOURCES = [
     id: 5,
     title: "whatfont",
     href: "https://chrome.google.com/webstore/detail/whatfont/jabopobgcpjmedljpbcaablpmlmfcogm",
-    resourceImg: day4,
+    resourceImg: singleGridCover,
     coverImg: singleGridCover,
     orientation: "single",
     releaseDate: "12/6/2023, 9:00:00 AM",
@@ -60,7 +62,7 @@ export const RESOURCES = [
     id: 6,
     title: "theleagueofimmoveabletype",
     href: "https://www.theleagueofmoveabletype.com/",
-    resourceImg: day5,
+    resourceImg: horizontalGridCover,
     coverImg: horizontalGridCover,
     orientation: "horizontal",
     releaseDate: "12/7/2025, 9:00:00 AM",
@@ -69,7 +71,7 @@ export const RESOURCES = [
     id: 7,
     title: "cardsforhumanity",
     href: "https://cardsforhumanity.frog.co/",
-    resourceImg: day6,
+    resourceImg: horizontalGridCover,
     coverImg: horizontalGridCover,
     orientation: "horizontal",
     releaseDate: "12/8/2025, 9:00:00 AM",
@@ -78,7 +80,7 @@ export const RESOURCES = [
     id: 8,
     title: "refero",
     href: "https://refero.design/",
-    resourceImg: "",
+    resourceImg: singleGridCover,
     coverImg: singleGridCover,
     orientation: "single",
     releaseDate: "12/11/2025, 9:00:00 AM",
@@ -87,7 +89,7 @@ export const RESOURCES = [
     id: 9,
     title: "logggos",
     href: "https://www.logggos.club/browse",
-    resourceImg: "",
+    resourceImg: singleGridCover,
     coverImg: singleGridCover,
     orientation: "single",
     releaseDate: "12/12/2025, 9:00:00 AM",
@@ -96,7 +98,7 @@ export const RESOURCES = [
     id: 10,
     title: "facts",
     href: "https://mywordle.strivemath.com/?word=svropwdomhsmlpfqpw",
-    resourceImg: "",
+    resourceImg: singleGridCover,
     coverImg: singleGridCover,
     orientation: "single",
     releaseDate: "12/13/2025, 9:00:00 AM",
@@ -105,7 +107,7 @@ export const RESOURCES = [
     id: 11,
     title: "weird-facts",
     href: "https://www.atlasobscura.com/",
-    resourceImg: "",
+    resourceImg: verticalGridCover,
     coverImg: verticalGridCover,
     orientation: "vertical",
     releaseDate: "12/14/2025, 9:00:00 AM",
@@ -114,7 +116,7 @@ export const RESOURCES = [
     id: 12,
     title: "attention-insight",
     href: "https://www.figma.com/community/plugin/968765016617421513/attention-insight",
-    resourceImg: "",
+    resourceImg: horizontalGridCover,
     coverImg: horizontalGridCover,
     orientation: "horizontal",
     releaseDate: "12/15/2025, 9:00:00 AM",
@@ -123,7 +125,7 @@ export const RESOURCES = [
     id: 13,
     title: "supertools",
     href: "https://supertools.therundown.ai/",
-    resourceImg: "",
+    resourceImg: verticalGridCover,
     coverImg: verticalGridCover,
     orientation: "vertical",
     releaseDate: "12/18/2025, 9:00:00 AM",
@@ -132,7 +134,7 @@ export const RESOURCES = [
     id: 14,
     title: "iconographic-encyclopedia",
     href: "https://www.c82.net/iconography/",
-    resourceImg: "",
+    resourceImg: horizontalGridCover,
     coverImg: horizontalGridCover,
     orientation: "horizontal",
     releaseDate: "12/19/2025, 9:00:00 AM",
@@ -141,7 +143,7 @@ export const RESOURCES = [
     id: 15,
     title: "never",
     href: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-    resourceImg: "",
+    resourceImg: singleGridCover,
     coverImg: singleGridCover,
     orientation: "single",
     releaseDate: "12/20/2025, 9:00:00 AM",
@@ -150,7 +152,7 @@ export const RESOURCES = [
     id: 16,
     title: "dopely",
     href: "https://colors.dopely.top/",
-    resourceImg: "",
+    resourceImg: singleGridCover,
     coverImg: singleGridCover,
     orientation: "single",
     releaseDate: "12/21/2025, 9:00:00 AM",
@@ -159,7 +161,7 @@ export const RESOURCES = [
     id: 17,
     title: "orion-icons",
     href: "https://www.orioniconlibrary.com/app",
-    resourceImg: "",
+    resourceImg: singleGridCover,
     coverImg: singleGridCover,
     orientation: "single",
     releaseDate: "12/22/2025, 9:00:00 AM",
