2d60a1f1057fc467238183205f35db68cc3eea3a
Dakota Fabro <dakota@weareoutright.com>
2023-12-04 13:47:51 -0800

    Update header

--- src/app/constants/marqueeMsgs.js
+++ src/app/constants/marqueeMsgs.js
@@ -1,11 +1,7 @@
-import OUTRIGHT from "../../app/Cursors_WreathHover.png";
-import STAR from "../../app/Cursors_Star.png";
-import SNOWFLAKE from "../images/snow/snowflake-single.svg";
-import LOGO from "../images/logo-ornament.svg";
-import Image from "next/image";
+// import OUTRIGHT from "../../app/Cursors_WreathHover.png";
+// import STAR from "../../app/Cursors_Star.png";
+// import SNOWFLAKE from "../images/snow/snowflake-single.svg";
+// import LOGO from "../images/logo-ornament.svg";
+// import Image from "next/image";
 
-export const MARQUEE_MSGS = [
-  <Image src={STAR} alt="" key="star" />,
-  "Happy Birthday, Antoine!",
-  <Image src={SNOWFLAKE} height={32} width={32} alt="" key="snowflake" />,
-];
+export const MARQUEE_MSGS = ["Happy Birthday, Antoine!"];
