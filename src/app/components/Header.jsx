e56dacd8c59198dca01acf584756155acc277205
Dakota Fabro <dakotafabrodev@gmail.com>
2023-12-04 09:51:36 -0800

    Update Header.jsx

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -4,12 +4,12 @@ import Marquee from 'react-fast-marquee'
 import { MARQUEE_MSGS } from '../constants/marqueeMsgs'
 
 const Header = () => {
-    const now = new Date(Date.now()).toLocaleString('en-us', {timeZone: 'America/New_York'})
+    const now = new Date(Date.now()).valueOf()
     const endOfAdvent = "12/22/2023, 9:00:00 AM"
 
   return (
     <div className='Header'>
-        <Marquee className={`Marquee ${now >= endOfAdvent ? "end-of-advent-marquee" : ""}`} speed={65} loop={0} pauseOnHover={true} autoFill={true}>
+        <Marquee className={`Marquee ${now >= new Date(endOfAdvent).valueOf() ? "end-of-advent-marquee" : ""}`} speed={65} loop={0} pauseOnHover={true} autoFill={true}>
             {MARQUEE_MSGS.map((msg, i) => {
                 return <p key={i} className="marquee-msg">{msg}</p>
             })}
