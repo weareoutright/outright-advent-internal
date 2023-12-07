34b9c7964fd466140ab88fa594fc1dc6d478675f
Dakota Fabro <dakota@weareoutright.com>
2023-12-07 09:33:35 -0800

    Add marquee messages

--- src/app/constants/marqueeMsgs.js
+++ src/app/constants/marqueeMsgs.js
@@ -1,7 +1,12 @@
-// import OUTRIGHT from "../../app/Cursors_WreathHover.png";
+import OUTRIGHT from "../../app/Cursors_WreathHover.png";
 // import STAR from "../../app/Cursors_Star.png";
 // import SNOWFLAKE from "../images/snow/snowflake-single.svg";
 // import LOGO from "../images/logo-ornament.svg";
-// import Image from "next/image";
+import Image from "next/image";
 
-export const MARQUEE_MSGS = ["It's an Outright holiday!"];
+export const MARQUEE_MSGS = [
+  "It's an Outright holiday!",
+  <Image src={OUTRIGHT} alt="" />,
+  "To Outright: I LOVE IT HERE. Thank you for giving me the workplace environment of my dreams 😭🫂 -Dakota",
+  <Image src={OUTRIGHT} alt="" />,
+];
