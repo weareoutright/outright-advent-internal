d17e3cd5e7a70f3816b44ade449833a73e38edb4
Dakota Fabro <dakota@weareoutright.com>
2023-12-07 11:38:59 -0800

    Add msgs

--- src/app/constants/marqueeMsgs.js
+++ src/app/constants/marqueeMsgs.js
@@ -7,7 +7,7 @@ import Image from "next/image";
 export const MARQUEE_MSGS = [
   "KEEP THAT CHANGE YA FILTHY ANIMALS",
   <Image key="outright-0" src={OUTRIGHT} alt="" />,
-  "To Outright: I LOVE IT HERE and I'm so thankful to be on this team! Thank you for giving me the workplace environment of my dreams 😭🫂 -Dakota",
+  "To Outright: I LOVE IT HERE -Dakota",
   <Image key="outright-1" src={OUTRIGHT} alt="" />,
   "Dear Sarah: We're doing great. -Gray",
   <Image key="outright-1" src={OUTRIGHT} alt="" />,
